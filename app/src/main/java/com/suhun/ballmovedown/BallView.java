593e0e23af99d69135071697c61753f674cf0615
suhunlin <lin.suhun@msa.hinet.net>
2023-04-22 23:18:53 +0800

    Add Scaled ball function by device monit width size

--- app/src/main/java/com/suhun/ballmovedown/BallView.java
+++ app/src/main/java/com/suhun/ballmovedown/BallView.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -14,16 +15,34 @@ public class BallView extends View {
     private String tag = BallView.class.getSimpleName();
     private Resources res;
     private Bitmap bmpball;
+    private boolean isInitBallInfo;
+    private int scalingRatio, ballW, ballH, ballX, ballY;
     public BallView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         res = context.getResources();
         setBackgroundResource(R.drawable.bg);
     }
 
+    private void initBallInfo(){
+        double scalingRatio = 12.0f;
+        int viewW = getWidth();
+        bmpball = BitmapFactory.decodeResource(res, R.drawable.ball1);
+        setScalingRatio(scalingRatio);
+        bmpball = Bitmap.createScaledBitmap(bmpball, ballW, ballH, false);
+        isInitBallInfo = true;
+    }
+
+    private void setScalingRatio(double ratio){
+        ballW = (int)(getWidth() / ratio);
+        ballH = ballW;
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        bmpball = BitmapFactory.decodeResource(res, R.drawable.ball1);
+        if(!isInitBallInfo){
+            initBallInfo();
+        }
         canvas.drawBitmap(bmpball, 0, 0, null);
     }
 }
