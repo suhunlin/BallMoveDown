a4e0189f0a497d114a97325874a9676062abb423
suhunlin <lin.suhun@msa.hinet.net>
2023-04-22 22:55:25 +0800

    Decode bmp file from drawable and draw the bmp ball by canvas in position x=0 and y=0

--- app/src/main/java/com/suhun/ballmovedown/BallView.java
+++ app/src/main/java/com/suhun/ballmovedown/BallView.java
@@ -1,14 +1,29 @@
 package com.suhun.ballmovedown;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class BallView extends View {
+    private String tag = BallView.class.getSimpleName();
+    private Resources res;
+    private Bitmap bmpball;
     public BallView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        res = context.getResources();
         setBackgroundResource(R.drawable.bg);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        bmpball = BitmapFactory.decodeResource(res, R.drawable.ball1);
+        canvas.drawBitmap(bmpball, 0, 0, null);
     }
 }
